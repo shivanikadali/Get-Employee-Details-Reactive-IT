e94a4ec0cbe19721b369cd397fca609e6a746cb5
shivanikadali <shivanikadali2@gmail.com>
2024-07-03 14:18:32 +0530

    starting project

---
+++ src/main/java/com/example/Get_Employee_details_Reactive/GetEmployeeDetailsReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.example.Get_Employee_details_Reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetEmployeeDetailsReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetEmployeeDetailsReactiveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/Get_Employee_details_Reactive/GetEmployeeDetailsReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Get_Employee_details_Reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetEmployeeDetailsReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
